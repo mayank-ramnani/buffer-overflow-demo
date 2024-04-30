5c17d39ca8377ad4ca7e6f6e70fb328d2f1c608c
mayank-ramnani <mayankr99@gmail.com>
2024-04-30 03:46:39 -0400

    add auth function; improve demo

--- main.c
+++ main.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define PASSWORD    "letmein"
-int main(void)
+#define PASSWORD    "secure"
+
+long int authenticate()
 {
     long int ret = 0;
     char flag[8] = {0};
     char buffer[8] = {0}; 
-    
+
     printf("Enter the password:\n");
     fgets(buffer, 16, stdin);
-    // make the 8th character null instead of newline
-    buffer[7] = 0;
 
-    if(strcmp(buffer, PASSWORD))
+    if(strncmp(buffer, PASSWORD, 6))
     {
         ret = -1;
         printf("Wrong Password\n");
@@ -30,8 +30,15 @@ int main(void)
         // give access priveleges to user
         printf ("------------------------\n");
         printf ("Access Granted!\n");
+        system("tail /etc/passwd");
         printf ("------------------------\n");
     }
+    return ret;
+}
 
+int main(void)
+{
+    long int ret = authenticate();
+    printf("Exiting with: %ld\n", ret);
     return ret;
 }
